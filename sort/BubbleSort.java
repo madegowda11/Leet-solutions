56b6b2fb645f6792e189b4f3d634c110f63e362a
Maadhu <140439336+madegowda11@users.noreply.github.com>
2026-06-07 10:49:52 +0530

    Update BubbleSort.java

--- sort/BubbleSort.java
+++ sort/BubbleSort.java
@@ -3,96 +3,51 @@ package com.Array.Package;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class BinarySearch {
-	static Scanner sc=new Scanner(System.in);
-	public static int[] createIntArray() {
-		int n,i;
-		System.out.println("Enter size: ");
-		n=sc.nextInt();
-		int[] arr=new int[n];
-		for(i=0;i<n;i++) {
-			System.out.print("enter the val");
-			arr[i]=sc.nextInt();
+public class BubbleSort {
+	public static int[] createIntArr() {
+		 int n,i;
+		 Scanner sc=new Scanner(System.in);
+			System.out.print("enter size:");
+			n=sc.nextInt();
+			int[] arr=new int[n];
+			for(i=0;i<arr.length;i++) {
+				System.out.print("enter val:");
+				arr[i]=sc.nextInt();
+			}
+			return arr;
 		}
-		return arr;
+	public static void swap(int[] arr,int left,int right) {
+		int temp=arr[left];
+		arr[left]=arr[right];
+		arr[right]=temp;
 	}
-
-	public static int binarySearchAsc(int[] arr, int target) {
-		int start=0, end=arr.length-1,mid;
-		while(start<=end) {
-			mid=start+((end-start)/2);
-			if(target == arr[mid]) {
-				return mid;
-			}
-			//probability logic
-			if(target < arr[mid]) {
-				end=mid-1;
-			}else {
-				start=mid+1;
-			}
-		}
-		return -1;
-	}
-	
-	public static int binarySearchDesc(int[] arr,int target) {
-		int st=0,end=arr.length-1,mid;
-		while(st<=end) {
-			mid=st+((end-st)/2);
-			if(target==arr[mid]) {
-				return mid;
-			}
-			//probability logic
-			if(target<arr[mid]) {
-				st=mid+1;
-			}else {
-				end=mid-1;
-			}
-		}
-		return -1;
-	}
-	
-	public static int orderAgnosticBinarySearch(int[] arr,int target) {
-		int  start=0,end=arr.length-1,mid;
-		String flag="asc";
-		if(arr[start]>arr[end]) {
-			flag="desc";
-		}
-		while(start<=end) {
-			mid=start+((end-start)/2);
-			if(target==arr[mid]) {
-				return mid;
-			}
-			if(flag.equals("asc")){
-				//probability logic
-				if(target < arr[mid]) {
-					end=mid-1;
-				}else {
-					start=mid+1;
-				}
-			}else {
-				//probability logic
-				if(target<arr[mid]) {
-					start=mid+1;
-				}else {
-					end=mid-1;
+	public static void bubbleSortAsc(int[] arr) {
+		int i,j,n=arr.length;
+		for(i=0;i<(n-1);i++) {
+			for(j=0;j<((n-1)-i);j++) {
+				if(arr[j]>arr[j+1]) {
+					swap(arr,j,(j+1));
 				}
 			}
 		}
-		return -1;
 	}
-	public static void main(String[] args) {
-		int[] arr=createIntArray();
-		System.out.println("Enter the target element to be searched: ");
-		int target=sc.nextInt();
-		System.out.println("original array"+Arrays.toString(arr));
-//		int index=binarySearchAsc(arr,target);
-//		int index=binarySearchDesc(arr,target);
-		int index=orderAgnosticBinarySearch(arr,target);
-		if(index==-1) {
-			System.out.println(target+" element is not found");
-		}else {
-			System.out.println(target+" element found at index "+index);
+	public static void bubbleSortDsc(int[] arr) {
+		int i,j,n=arr.length;
+		for(i=0;i<(n-1);i++) {
+			for(j=0;j<((n-1)-i);j++) {
+				if(arr[j]<arr[j+1]) {
+					swap(arr,j,(j+1));
+				}
+			}
 		}
 	}
-
+	public static void main(String[]args) {
+		int[] arr=createIntArr();
+		System.out.println("original arrays:"+Arrays.toString(arr));
+		bubbleSortAsc(arr);
+		System.out.println("sorted ascending order:"+Arrays.toString(arr));
+		bubbleSortDsc(arr);
+		System.out.println("Dsc sorted array:"+Arrays.toString(arr));
+	}
 }
+
